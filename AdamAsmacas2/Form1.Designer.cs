0a2ca4e77e87b9635647944a9c986d26ed6336ed
hackk <hackk@DESKTOP-UL7T61J>
2022-12-30 18:33:41 +0300

    Add project files.

---
+++ AdamAsmacas2/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace AdamAsmacas2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(341, 7);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 17);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(342, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Kelime Oluşturma";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(468, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(342, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(342, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 28);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Kelime Tahmin";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(469, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(342, 127);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 28);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Harf Tahmin";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::AdamAsmacas2.Properties.Resources.AdamAsmaca1;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(324, 307);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "AdamAsmaca1.png");
+            this.imageList1.Images.SetKeyName(1, "AdamAsmaca2.png");
+            this.imageList1.Images.SetKeyName(2, "AdamAsmaca3.png");
+            this.imageList1.Images.SetKeyName(3, "AdamAsmaca4.png");
+            this.imageList1.Images.SetKeyName(4, "AdamAsmaca5.png");
+            this.imageList1.Images.SetKeyName(5, "AdamAsmaca6.png");
+            this.imageList1.Images.SetKeyName(6, "AdamAsmaca7.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ AdamAsmacas2/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdamAsmacas2
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        string[] kelimeler = new string[] {"YAVUZ", "UÇAK", "GEMİ", "MEKANİK", "ELEKTRONİK", "MERHABA", "KODLAMA", "İZMİR", "BOYOZ", "KARTAL", "BİLGİSAYAR", "KUŞ"};
+        public string rastgeleKelime;
+        public int rastgele;
+        public string[] harf;
+        public int imageNumber = 0;
+        public int can;
+        
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            rastgele = rnd.Next(0, kelimeler.Length);
+            rastgeleKelime = kelimeler[rastgele];
+            rastgeleKelime = rastgeleKelime.ToUpper();
+            listBox1.Items.Add(rastgeleKelime);
+            harf = new string[rastgeleKelime.Length];
+            can = 6;
+            label1.Text = " ";
+            label2.Text = can.ToString();
+            for (int i = 0; i < rastgeleKelime.Length; i++)
+            {
+                harf[i] = "_";
+                label1.Text += harf[i] + " ";
+            }
+        }
+        public string girilenKelime; 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            string girilenKelime = textBox1.Text.ToUpper();
+
+            if (girilenKelime == rastgeleKelime)
+            {
+                for (int j = 0; j < rastgeleKelime.Length; j++)
+                {
+                    harf[j] = girilenKelime[j].ToString();
+                }
+            }            
+            else
+            {
+                can--;
+                imageNumber++;
+                pictureBox1.Image = imageList1.Images[imageNumber];
+                
+                if(can == 0)
+                {
+                    MessageBox.Show("Kaybettiniz", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    label1.Text = "";
+                }
+            }            
+            if (rastgeleKelime == girilenKelime)
+            {
+                MessageBox.Show("Kazandınız", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                label1.Text = "";
+            }
+            label2.Text = can.ToString();
+            label1.Text = " ";
+            for (int j = 0; j < rastgeleKelime.Length; j++)
+            {
+                label1.Text += harf[j] + " ";
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string girilenKelime = textBox1.Text.ToUpper();
+            bool döngü = false;
+            for (int i = 0; i < rastgeleKelime.Length; i++)
+            {
+                if (girilenKelime[0] == rastgeleKelime[i])
+                {
+                    harf[i] = girilenKelime[0].ToString();
+                    döngü = true;
+                    
+                }
+                
+            }
+            if (döngü == false)
+            {
+                can--;
+                imageNumber++;
+                pictureBox1.Image = imageList1.Images[imageNumber];
+            }
+            label2.Text = can.ToString();
+            if (can == 0)
+            {
+                MessageBox.Show("Kaybettiniz", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                label1.Text = "";
+            }
+            if (rastgeleKelime == girilenKelime)
+            {
+                MessageBox.Show("Kazandınız", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                label1.Text = "";
+            }
+            label1.Text = " ";
+            for (int j = 0; j < rastgeleKelime.Length; j++)
+            {
+                label1.Text += harf[j] + " ";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = imageList1.Images[imageNumber];
+        }
+    }
+}
